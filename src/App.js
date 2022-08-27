6e0482df606c1b6713dd21759c2cf034dcda55c3
Ibrahimnalmanaseer <ibrahimnalmanaseer@gmail.com>
2022-08-27 17:32:36 +0300

    add my personal info

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     var resumePath =
       document.documentElement.lang === window.$primaryLanguage
         ? `res_primaryLanguage.json`
-        : `res_secondaryLanguage.json`;
+        : `res_primaryLanguage.json`;
     this.loadResumeFromPath(resumePath);
   }
 
@@ -87,6 +87,7 @@ class App extends Component {
             element={<Home
               resumeData={this.state.resumeData}
               sharedData={this.state.sharedData}
+              
             />}>
           </Route>
           <Route 
